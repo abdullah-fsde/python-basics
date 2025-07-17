857c66b24f45b59462458cad97739e07c95e6000
Mohammed Abdullah <abdullahfsde@gmail.com>
2025-07-17 09:16:43 +0530

    Day 1 – Python Basics & Practice
    ✔ Learned Python modules, pip, REPL, and comments  
    ✔ Wrote 5 programs:
       1. Hello World  
       2. Print poem line by line  
       3. Simple calculator  
       4. Directory listing using os module  
       5. Imported and used modules + comments
    🚀 First commit on the journey to becoming a Full Stack Data Engineer 💼

---
+++ 02_ps1_poem.py
@@ -0,0 +1,12 @@
+print("twinkle twinkle little star \n" \
+"how i wonder what you are \n" \
+"up above the world so high \n" \
+"like a diamond in the sky")
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.''')
+''' there are 2 ways to print the same string in python
+1. using \n to create a new line    
+2. using triple quotes ''' ''' to create a multi-line string
+'''
