40e2bccd8be5fb45d8c41077da586187aae81965
Mohammed Abdullah <abdullahfsde@gmail.com>
2025-07-17 13:45:21 +0530

    Delete 06_ps2_remainder.py
    Cleaned up unstructured files to re-organize chapter-wise

--- 06_ps2_remainder.py
+++
@@ -1,7 +0,0 @@
-a = int(input("enter a number: "))
-b = int(input("enter another number: "))
-result = a%b
-print("The remainder of",a,"and",b,"is",result)
-print("The type of the result is", type(result))
-result2 = a % 2
-print("The remainder of", a, "when divided by 2 is", result2)
